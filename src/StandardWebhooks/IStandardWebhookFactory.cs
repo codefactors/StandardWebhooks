825ee51c8653432b3facfe789d3b18cc42be9cf4
payetools <143715134+payetools@users.noreply.github.com>
2024-09-14 06:40:57 +0200

    feat: added factory for standard webhooks, especially useful for di scenarios

---
+++ src/StandardWebhooks/IStandardWebhookFactory.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) 2024, Codefactors Ltd.
+//
+// Codefactors Ltd licenses this file to you under the following license(s):
+//
+//   * The MIT License, see https://opensource.org/license/mit/
+
+namespace StandardWebhooks;
+
+/// <summary>
+/// Interface for factories that can create <see cref="StandardWebhook"/> instances.
+/// </summary>
+/// <remarks>Intended particularly for use in ASP.NET dependency injection scenarios.</remarks>
+public interface IStandardWebhookFactory
+{
+    /// <summary>
+    /// Creates a new instance of a <see cref="StandardWebhook"/>.
+    /// </summary>
+    /// <returns>New instance of a <see cref="StandardWebhook"/>.</returns>
+    StandardWebhook CreateWebhook();
+}
